ef930d58eacdda865d62e35498dc7922f6a263d3
Marrubi1 <91191130+Marrubi1@users.noreply.github.com>
2023-05-31 21:13:24 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Clock from "./clock";
 
 function App() {
-  return <div></div>;
+
+  return (
+    <div>
+      <Clock city={"Stockholm"} timeZone={"Europe/Stockholm"} />
+      <Clock city={"Shanghai"} timeZone={"Asia/Shanghai"} />
+      <Clock city={"Zagreb"} timeZone={"Europe/Zagreb"} />
+    </div>
+  );
 }
 
 export default App;
